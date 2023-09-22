4af4bcc1c5ad27ffd83dc9ad1ec6bc51be026808
luther landry <landry@varnsen.net>
2023-09-21 22:21:39 -0500

    Upgrade pip before installing requirements

--- setup.sh
+++ setup.sh
@@ -36,6 +36,8 @@ if [ ! -d "venv" ]; then
     fancy_echo "Creating virtualenv"
     python3 -m venv venv
     source venv/bin/activate
+    pip install --upgrade pip
+    pip install wheel
     pip install -r requirements.txt
 fi
 
