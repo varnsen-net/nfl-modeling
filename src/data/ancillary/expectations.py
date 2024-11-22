4a7c04148cee411963f93c8ecbea037e74e87b91
luther landry <landry@varnsen.net>
2024-11-21 19:23:11 -0600

    Clean up expected value calculator module

--- src/data/ancillary/expectations.py
+++ src/data/ancillary/expectations.py
@@ -1,4 +1,4 @@
-""""""
+"""Helper functions to calculate expected values for field position."""
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -82,17 +82,25 @@ def calulate_mean_yrdln_values(plays):
             .mean())
 
 
-if __name__ == '__main__':
-    raw_plays_path = PATHS['raw_plays']
-    expected_values_path = PATHS['expected_values']
-    all_plays = pd.DataFrame()
-    expected_values = pd.DataFrame()
+def do_regression_analysis(yrdln_values):
+    """Perform regression analysis on yardline values.
+    
+    :param pd.DataFrame yrdln_values: Yardline values.
+    :return: Predicted values.
+    :rtype: np.ndarray
+    """
+    regr = LinearRegression()
+    regr.fit(yrdln_values[['absolute_yrdln']], yrdln_values['net_value'])
+    return regr.predict(yrdln_values[['absolute_yrdln']])
 
-    seasons = list(range(2001, 2025))
-    all_plays = pd.concat([pd.read_parquet(raw_plays_path / f"play_by_play_{season}.parquet")
-                           for season in seasons])
-    all_plays = preprocess_plays(all_plays)
 
+def calculate_field_position_ev(all_plays):
+    """Calculate expected values for field position.
+
+    :param pd.DataFrame all_plays: All play-by-play data.
+    :return: Expected values for field position.
+    :rtype: pd.DataFrame
+    """
     types = ['home', 'away']
     downs = [1] # this is a list so we can add more downs later
     grid = [(type, down) for type in types for down in downs]
@@ -110,20 +118,25 @@ if __name__ == '__main__':
                       'result', 'posteam_result', 'yrdln_fixed', 'net_value',
                       'absolute_yrdln']]
 
-        yrdln_values = calulate_mean_yrdln_values(plays)
-        yrdln_values = (yrdln_values
-                        .reset_index()
-                        .assign(posteam_type=type,
-                                down=down))
+        expected_values = (calulate_mean_yrdln_values(plays)
+                           .reset_index()
+                           .assign(posteam_type=type,
+                                   down=down)
+                           .assign(expected_value=do_regression_analysis))
+        yield expected_values
 
 
-        regr = LinearRegression()
-        regr.fit(yrdln_values[['absolute_yrdln']], yrdln_values['net_value'])
-        yrdln_values['expected_value'] = regr.predict(yrdln_values[['absolute_yrdln']])
-        expected_values = pd.concat([expected_values, yrdln_values])
+if __name__ == '__main__':
+    raw_plays_path = PATHS['raw_plays']
+    expected_values_path = PATHS['expected_values']
 
-    print(expected_values)
+    all_plays = pd.DataFrame()
+    seasons = list(range(2001, 2025))
+    all_plays = pd.concat([pd.read_parquet(raw_plays_path / f"play_by_play_{season}.parquet")
+                           for season in seasons])
+    all_plays = preprocess_plays(all_plays)
 
+    expected_values = pd.concat(calculate_field_position_ev(all_plays))
     expected_values.to_csv(expected_values_path, index=False)
 
     fig, ax = plt.subplots()
